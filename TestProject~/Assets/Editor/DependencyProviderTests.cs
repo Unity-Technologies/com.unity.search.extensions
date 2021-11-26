facdd5261fd62a50547601d6de0f7cef5e509625
Jonathan Schmidt <jonathans@unity3d.com>
2021-11-26 10:45:12 -0500

    Fix whitespaces

--- TestProject~/Assets/Editor/DependencyProviderTests.cs
+++ TestProject~/Assets/Editor/DependencyProviderTests.cs
@@ -5,11 +5,11 @@ using UnityEngine.TestTools;
 
 class DependencyProviderTests
 {
-	public struct TestCase
-	{
-		public readonly string query;
-		public readonly string[] expectedIds;
-		public readonly string[] expectedLabels;
+    public struct TestCase
+    {
+        public readonly string query;
+        public readonly string[] expectedIds;
+        public readonly string[] expectedLabels;
 
         public TestCase(string query, string[] expectedIds = null, string[] expectedLabels = null)
         {
@@ -19,7 +19,7 @@ class DependencyProviderTests
         }
 
         public TestCase(string query, string expectedValue, bool isLabel = false)
-			: this(query, isLabel ? null : new string[] { expectedValue }, isLabel ? new string[] { expectedValue } : null)
+            : this(query, isLabel ? null : new string[] { expectedValue }, isLabel ? new string[] { expectedValue } : null)
         {
         }
 
@@ -29,51 +29,51 @@ class DependencyProviderTests
         }
     }
 
-	SearchProvider provider;
-	static TestCase[] testCases = new TestCase[]
-	{
-		new TestCase("e7969613e431dd449966876222fc5d21", "Assets/Materials/Red.mat", isLabel: true),
-		new TestCase("is:file -is:package", "Assets/Editor/com.unity.search.extensions.tests.asmdef", isLabel: true),
+    SearchProvider provider;
+    static TestCase[] testCases = new TestCase[]
+    {
+        new TestCase("e7969613e431dd449966876222fc5d21", "Assets/Materials/Red.mat", isLabel: true),
+        new TestCase("is:file -is:package", "Assets/Editor/com.unity.search.extensions.tests.asmdef", isLabel: true),
         new TestCase("is:folder -is:package", "Assets/Prefabs", isLabel: true),
         new TestCase("is:broken", "Assets/Runtime/ManyRefs.asset", isLabel: true),
         new TestCase("is:missing in=1", "Packages/com.unity.search.extensions", isLabel: true), // We do not map folders?
-		new TestCase("from=Assets/Runtime/ManyRefs.asset", "Assets/Prefabs/Simple.prefab", isLabel: true),
-		new TestCase("from=Assets/Runtime/ManyRefs.asset in=1", "Assets/Scripts/ManyRefs.cs", isLabel: true),
-		new TestCase("from=[Assets/Runtime/ManyRefs.asset]", "10dc1e46f5f3dda43938758225fafe87"),
-		new TestCase("ref=Assets/Materials/Red.mat", "345b0b890043f484095fc55e158702b4"),
+        new TestCase("from=Assets/Runtime/ManyRefs.asset", "Assets/Prefabs/Simple.prefab", isLabel: true),
+        new TestCase("from=Assets/Runtime/ManyRefs.asset in=1", "Assets/Scripts/ManyRefs.cs", isLabel: true),
+        new TestCase("from=[Assets/Runtime/ManyRefs.asset]", "10dc1e46f5f3dda43938758225fafe87"),
+        new TestCase("ref=Assets/Materials/Red.mat", "345b0b890043f484095fc55e158702b4"),
         new TestCase("out>5", "85afa418919e4626a5688f4394b60dc4"),
 
-		new TestCase("dep:in=0 is:file -is:package", "Assets/Editor/DependencyProviderTests.cs", isLabel: true),  // Unused assets
+        new TestCase("dep:in=0 is:file -is:package", "Assets/Editor/DependencyProviderTests.cs", isLabel: true),  // Unused assets
 
-		new TestCase("first{25,sort{select{p:a:assets, @path, count{dep:ref=\"@path\"}}, @value, desc}}", null, new string[]
-		{
-			"Assets/Prefabs/Simple.prefab",
-			"Assets/Materials/Red.mat",
-			"Assets/Runtime/ManyRefs.asset"
-		})
-	};
+        new TestCase("first{25,sort{select{p:a:assets, @path, count{dep:ref=\"@path\"}}, @value, desc}}", null, new string[]
+        {
+            "Assets/Prefabs/Simple.prefab",
+            "Assets/Materials/Red.mat",
+            "Assets/Runtime/ManyRefs.asset"
+        })
+    };
 
-	[UnitySetUp]
-	public IEnumerator BuildDatabase()
-	{
-		Dependency.Build();
-		while (!Dependency.IsReady())
-			yield return null;
+    [UnitySetUp]
+    public IEnumerator BuildDatabase()
+    {
+        Dependency.Build();
+        while (!Dependency.IsReady())
+            yield return null;
 
-		provider = SearchService.GetProvider(Dependency.providerId);
-	}
+        provider = SearchService.GetProvider(Dependency.providerId);
+    }
 
-	[UnityTest]
-	public IEnumerator Query([ValueSource(nameof(testCases))] TestCase testCase)
-	{
-		using (var context = SearchService.CreateContext(provider, testCase.query))
-		using (var results = SearchService.Request(context))
-		{
-			while (results.pending)
-				yield return null;
+    [UnityTest]
+    public IEnumerator Query([ValueSource(nameof(testCases))] TestCase testCase)
+    {
+        using (var context = SearchService.CreateContext(provider, testCase.query))
+        using (var results = SearchService.Request(context))
+        {
+            while (results.pending)
+                yield return null;
 
-			if (testCase.expectedIds != null)
-				CollectionAssert.IsSubsetOf(testCase.expectedIds, results.Select(r => r.id));
+            if (testCase.expectedIds != null)
+                CollectionAssert.IsSubsetOf(testCase.expectedIds, results.Select(r => r.id));
 
             if (testCase.expectedLabels != null)
                 CollectionAssert.IsSubsetOf(testCase.expectedIds, results.Select(r => r.id));
